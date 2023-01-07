1b1c2843f8e602ffc38621e910ed9841b75b566f
Qasim Khawaja <q.khawaja@yahoo.com>
2023-01-07 12:00:00 -0700

    initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import axios from 'axios';
+import {Repository, GitExtension} from './git';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,13 +14,61 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commitgen.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CommitGen!');
-	});
+	let disposable = vscode.commands.registerCommand('commitgen.genMessage', (repository: Repository) => {
+		repository = getRepo(repository);
+		repository.diff(true).then((data) => {
+			const files = data.split('diff --git');
+			// For each file remove until the first @@
+			const filesWithoutHeader = files.map((file,i) => {
+				const filePathA = file.substring(file.indexOf('a/'), file.indexOf(' b/')).replace('a/', '');
+				const filePathB = file.substring(file.indexOf('b/'), file.indexOf('\n')).replace('b/', '');
+				const code:string = file.substring(file.indexOf('@@')) || '';
+				return code.length ? ` file path A: ${filePathA} | file path B: ${filePathB}\n\n${code}` : '';
+			}).filter((file) => file !== '');
+			let prompt = filesWithoutHeader.join('\n---\n');
+			prompt+= "\n\n###\n\n";
+			axios({
+				method: 'post',
+				url: 'https://api.openai.com/v1/completions',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Bearer <KEY>'
+				},
+				data: {
+					"model": "curie:ft-personal:commit-2023-01-07-01-35-45",
+					"prompt": prompt,
+					"temperature": 0.8,
+					"max_tokens": 50,
+					"top_p": 1,
+					"frequency_penalty": 0,
+					"presence_penalty": 0,
+					"stop": ["\n"]
+				  }
+			}).then((response) => {
+				const commitMessage = response.data.choices[0].text;
+				repository.inputBox.value = commitMessage;
+			});
+				
+			
+		});
+	}, { repository: true });
 
 	context.subscriptions.push(disposable);
+	vscode.commands.executeCommand('setContext', 'commitGen.enabled', true);
+}
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) vscode-whatthecommit. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *---------*/
+function getRepo(repository: Repository | undefined): Repository {
+	if (repository === undefined) {
+		let gitExtension = vscode?.extensions?.getExtension<GitExtension>('vscode.git')?.exports;
+		let api = gitExtension?.getAPI(1);
+		repository = api?.repositories[0];
+	}
+
+	return repository as Repository;
 }
 
 // This method is called when your extension is deactivated
